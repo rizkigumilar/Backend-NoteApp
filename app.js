124a24478319734878039a3efb49eea0bf4487a5
Rizki Gumilar <rizgumm@gmail.com>
2019-09-02 20:14:20 +0700

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const app = express()
-const port = process.env.SERVER_PORT || 5000
+const port = process.env.PORT || 5000
 
 const noteRoute = require('./src/route/routes')
 
